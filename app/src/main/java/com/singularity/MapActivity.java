5be1f24ae46ad3f970b54c611f8f5d9f44c88cd9
siddhartha <siddhartha.maji@bluehorse.in>
2018-04-05 14:49:46 +0530

    Map added

--- app/src/main/java/com/singularity/MapActivity.java
+++ app/src/main/java/com/singularity/MapActivity.java
@@ -1,13 +1,66 @@
 package com.singularity;
 
+import android.animation.Animator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private  SupportMapFragment mapFragment;
+    private FrameLayout flLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        initialize();
+        showMap();
+    }
+
+    private void initialize() {
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        flLoader = findViewById(R.id.flLoader);
+
+    }
+
+    private void showMap() {
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        //Toast.makeText(this, "Map Loaded!", Toast.LENGTH_SHORT).show();
+        animateMapApear();
+    }
+
+    private void animateMapApear() {
+        flLoader.animate().alpha(0).setDuration(2000).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                flLoader.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        });
     }
 }
